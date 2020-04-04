2c83b780c33f2442548f8879f59688658d2498f4
sankets <sahasra.sanket@gmail.com>
2020-04-04 11:36:52 +0530

    Shift and acc in ParseTable

--- table_generator.py
+++ table_generator.py
@@ -6,7 +6,7 @@ Created on Fri Apr  3 15:47:16 2020
 """
 
 #PARSER table and goto table
-f1 = open('first.txt','r').read().splitlines()
+f1 = open('./Text Files/first.txt','r').read().splitlines()
 first = dict({})
 for i in f1:
 	x,y=i.split(':')
@@ -14,19 +14,19 @@ for i in f1:
 	first[x]=y
 del f1
 
-f2 = open('follow.txt','r').read().splitlines()
+f2 = open('./Text Files/follow.txt','r').read().splitlines()
 follow = dict({})
 for i in f2:
 	x,y=i.split(':')
 	follow[x]=y
 del f2
 #%%
-f3 = open('columns.txt','r').read().splitlines()
+f3 = open('./Text Files/columns.txt','r').read().splitlines()
 columns = []
 for i in f3:
 	columns.append(i)
 
-f4 = open('itemset.txt','r').read().splitlines()
+f4 = open('./Text Files/itemset.txt','r').read().splitlines()
 itemset = dict([])
 for i in f4:
 	x,y=i.split(':')
@@ -39,7 +39,7 @@ for i in f4:
 		tempdict[k]=v
 	itemset[x]=tempdict
 
-f5 = open('gototab.txt','r').read().splitlines()
+f5 = open('./Text Files/gototab.txt','r').read().splitlines()
 gototab = dict([])
 for i in f5:
 	x,y=i.split(':')
@@ -53,14 +53,24 @@ for i in f5:
 	gototab[x]=tempdict
 
 productions = dict([])
-f6 = open('productions.txt','r').read().splitlines()
+f6 = open('./Text Files/productions.txt','r').read().splitlines()
 for i in f6:
 	print(i)
 	x,y = i.split(':')
 	productions[x]=y
 
 del f3,f4,f5,f6,i,item,k,v,x,y,temp,tempdict
+#parsetable.columns = columns
+#print(parsetable)
+
 #%%
+def getProductionNum(src,dest):
+	new = src+' -> '+dest[:-1]
+	print(src,dest)
+	for k,v in productions.items():
+		if v==new:
+			return k
+
 import pandas as pd
 columns.extend(['E','T','F'])
 parsetable = pd.DataFrame(columns=columns)
@@ -86,17 +96,21 @@ for i in range(len(gototab)):
 	#print(temp)
 	parsetable.loc[i]=temp
 del i,k,name,temp
+#
 #%%
-def getProductionNum(src,dest):
-	new = src+' -> '+dest[:-1]
-	print(src,dest)
-	for k,v in productions.items():
-		if v==new:
-			return k
+# setting accept entries in parsetable
+for state,prods in itemset.items():
+	
+	for k,v in prods.items():
+		
+		if k.rstrip("'")==v.rstrip('.'):
+			parsetable.at[int(state.lstrip('I')),'$']='acc'
+	
 
-#%%	
+
+#%%
 for k,y in follow.items():
-	print(k)
+	#print(k)
 	for state,prods in itemset.items():
 		for src,dest in prods.items():
 			temp = dest.split('|')
@@ -108,18 +122,18 @@ for k,y in follow.items():
 					which = getProductionNum(src,item)
 					#print(which)
 					new = follow[k].replace("'",'')
-					new = follow[k].replace("{",'')
-					new = follow[k].replace("}",'')
-					new = follow[k].replace(",",'')
+					new = new.replace("{",'')
+					new = new.replace("}",'')
+					new = new.replace(",",'')
+					print(new)
 					for i in new:
-						parsetable.at[int(state.lstrip('I')),i]='r'+str(which)
+						#get=parsetable.at[int(state.lstrip('I')),i]
+						#print(get)
+						if which!=None: #and get!=r'-':
+							parsetable.at[int(state.lstrip('I')),i]='r'+str(which)
 #%%
-		
-	
-	
-	
-
-
-
-
-
+parsetable=parsetable.drop(parsetable.columns[-1],axis=1)
+#%%	
+ptdict = parsetable.to_dict()
+ptcsv = parsetable.to_csv('./Text Files/ptcsv.csv',header=True,index=False)					
+					
